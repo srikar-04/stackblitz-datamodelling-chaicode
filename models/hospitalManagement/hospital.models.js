55b8e4ab7b3dc31d8703b4cdf79cc27405fbb373
srikar-04 <srikar.code01@gmail.com>
2025-01-16 10:38:58 +0530

    added hospital model

--- models/hospitalManagement/hospital.models.js
+++ models/hospitalManagement/hospital.models.js
@@ -1,5 +1,29 @@
 import mongoose from 'mongoose'
 
-const hospitalSchema = new mongoose.Schema({},{timestamps: true});
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    adress: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    specializedIn: [
+        {
+            type: String,
+            required: true
+        }
+    ],
+    
+},{timestamps: true});
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema)
